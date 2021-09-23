534bb37f7af1a3246680d75a9c27e6eb9ef1c69f
Estebanm1812 <EstebanMendoza02@hotmail.com>
2021-09-23 18:52:52 -0500

    Thread Created, needs to be implemented

--- src/thread/GameStoreThread.java
+++ src/thread/GameStoreThread.java
@@ -1,6 +1,26 @@
 package thread;
 
+import model.Store;
+
 public class GameStoreThread extends Thread{
 
+	private Store gameStore;
 	
+	private long sleepTime;
+	
+	public GameStoreThread(Store gameStore,long sleepTime) {
+		
+		this.gameStore = gameStore;
+		this.sleepTime = sleepTime;
+	}
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e1) {
+			
+			e1.printStackTrace();
+		}
+		
+	}	
 }
